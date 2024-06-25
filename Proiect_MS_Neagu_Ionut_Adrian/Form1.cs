6c5668d05fe141a4f1daf2a657ac46c7a3c4f609
Zapacitul46 <ion_ion55@yahoo.com>
2024-06-25 12:56:00 +0300

    first commit

---
+++ Proiect_MS_Neagu_Ionut_Adrian/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proiect_MS_Neagu_Ionut_Adrian
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        double f(double x)
+        {
+            return 1 / x;
+        }
+
+        double Iexact(double a, double b)
+        {
+            return Math.Log(b) - Math.Log(a);
+        }
+
+            private void button1_Click(object sender, EventArgs e)
+        {
+
+                double a, b, U, I, S;
+                int n, i;
+                Random r = new Random();
+                a = Convert.ToDouble(textBox1.Text);
+                b = Convert.ToDouble(textBox2.Text);
+                n = Convert.ToInt16(textBox3.Text);
+                S = 0;
+                for (i = 1; i <= n; i++)
+                {
+                    U = r.NextDouble();
+                    S += f(a + (b - a) * U);
+                }
+                I = (b - a) * S / n;
+                textBox4.AppendText("\r\n I exact = " + Iexact(a,b).ToString("0.000000"));
+                textBox4.AppendText("\r\n I aprox = " + I.ToString("0.000000"));
+
+        }
+    }
+}
